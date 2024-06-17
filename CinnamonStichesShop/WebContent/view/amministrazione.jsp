37a800a2044995da4a63410c2975ef97ccc7a6d4
emanueleivn <e.iovane2@studenti.unisa.it>
2024-06-17 15:19:30 +0200

    conesioni servlet jsp amministratore

--- CinnamonStichesShop/WebContent/view/amministrazione.jsp
+++ CinnamonStichesShop/WebContent/view/amministrazione.jsp
@@ -13,16 +13,16 @@
 <body>
 	<div class="container">
 		<div class="bottoni">
-		<form action="${pageContext.request.contextPath}/PaginaAmministratore?val=add" method="GET">
+		<form action="${pageContext.request.contextPath}/PaginaAmministratore?azione=add" method="GET">
 			<button type="submit" name="aggiungi">Aggiungi prodotto</button>
 		</form>
-			<form action="${pageContext.request.contextPath}/PaginaAmministratore?val=mod" method="GET">
+			<form action="${pageContext.request.contextPath}/PaginaAmministratore?azione=mod" method="GET">
 			<button type="submit" name="modifica">Modifica prodotto</button>
 		</form>
-			<form action="${pageContext.request.contextPath}/PaginaAmministratore?val=showOrders" method="GET">
+			<form action="${pageContext.request.contextPath}/PaginaAmministratore?azione=showOrders" method="GET">
 			<button type="submit" name="Logout">Visualizza ordini</button>
 		</form>
-			<form action="${pageContext.request.contextPath}/PaginaAmministratore?val=showUsers" method="GET">
+			<form action="${pageContext.request.contextPath}/PaginaAmministratore?azione=showUsers" method="GET">
 			<button type="submit" name="Logout">Visualizza utenti</button>
 		</form>
 			
--- CinnamonStichesShop/src/control/Admin/RedirAdminServlet.java
+++ CinnamonStichesShop/src/control/Admin/RedirAdminServlet.java
@@ -15,27 +15,41 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/PaginaAmministratore")
 public class RedirAdminServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public RedirAdminServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/view/amministrazione.jsp");
-		dispatcher.forward(request, response);
+	public RedirAdminServlet() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String azione = request.getParameter("azione");
+		String reAddress ="";
+		switch (azione) {
+		case "add":reAddress="/view/aggiungiProdotto.jsp";
+		case "mod":reAddress="/view/modificaProdotto.jsp";
+		case "showOrders":reAddress="/view/ordini.jsp";
+		case "showUsers":reAddress="/view/utenti.jsp";
+		default: reAddress= "/view/amministrazione.jsp";
+		}
+		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(reAddress);
+		dispatcher.forward(request, response);
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doGet(request, response);
 	}
 
